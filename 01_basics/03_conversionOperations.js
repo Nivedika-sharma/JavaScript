6ed24f51cd79c18a4fc91ecf061858a3936bb784
Nivedika-sharma <147430399+Nivedika-sharma@users.noreply.github.com>
2025-02-04 18:13:17 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+// let score=33
+// console.log(typeof score);
+score="33"
+console.log(typeof score);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+
+
+marks="20abc"
+console.log(typeof marks);
+let marksInNumber=Number(marks);
+console.log(typeof marksInNumber) //gives number hence there is a problem so we need to check properly
+
+//"33"=>33
+//"23dd"=>Nan
+//true=>1;false=>0
+
+let isloggedIn=1
+let booleanloggedIn=Boolean(isloggedIn);
+console.log(typeof booleanloggedIn)
+
+let num=33
+let stringnum=String(num)
+console.log(typeof stringnum)
+console.log(typeof num)
