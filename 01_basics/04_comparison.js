16ea38608d60763dbc09b57ca480de61186d325b
Nivedika-sharma <147430399+Nivedika-sharma@users.noreply.github.com>
2025-02-04 18:50:47 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2==3)
+// console.log(2>=3)
+// console.log(3<=3)
+// console.log(2!=3)
+// console.log(2===7)//strictly equal to
+
+//*****//
+//datatype must be same
+
+//equality check and comparison operator works differently ..comparison convert null to a number,treating it as 0
+console.log(null==0)
+console.log(null!=0)
+console.log(null<=0)
+console.log(null>=0)
+console.log(null>0)
+
