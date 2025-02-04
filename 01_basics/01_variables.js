188e0bb38f28fd0fcbb3c4d0ecbdd7bce241a58e
Nivedika-sharma <147430399+Nivedika-sharma@users.noreply.github.com>
2025-02-04 17:32:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 14453//cannot be modified nor redeclared
+let accountEmail= "niv@gmail.com" //can be modified but not redeclared
+var accountPassword ="2553454" //can be redeclared and modified but prefer not to use due to issue in functional and block scope
+accountCity ="Jaipur" 
+//accountState;
+
+accountID=1 // cannot modify
+
+console.log(accountId);
+
+accountEmail="gfhf@gmail.com"
+
+console.log(accountEmail);
+
+accountPassword="577"
+
+console.log(accountPassword);
+
+accountCity="Bangaluru"
+
+console.log(accountCity);
+
+console.table([accountEmail,accountPassword,accountId,accountCity]);
