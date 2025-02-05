5716690ee1cbb1ec6b38226bcb4e6274ab183fe1
Nivedika-sharma <147430399+Nivedika-sharma@users.noreply.github.com>
2025-02-05 17:30:53 +0000

    dataTypes-summary

---
+++ 01_basics/dataTypes-Summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+//7 types: String ,Number,Boolean,null,undefined,Symbol,BigInt
+const score=100
+const scoreValue=11100.3
+
+const isLoggedIn =false
+const temp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+const bigNum=234445566456575n
+
+
+//Reference Type(Non-primitive)
+
+//array,Objects,Functions
+
+const fruits=["mango","apple","guava"];
+const obj={
+    Name:"Nivedika",
+    Age:20,
+}
+
+const myfn=function(){
+    console.log("HELLO");
+}
+console.log(typeof score ,typeof scoreValue,typeof isLoggedIn,typeof temp,typeof userEmail,typeof id,typeof anotherId);
+
+console.log(typeof fruits,typeof obj,typeof myfn);
