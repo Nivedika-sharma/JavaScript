a060842dcad111b4f4ef0f98c5663a2ca56f75c2
Nivedika-sharma <147430399+Nivedika-sharma@users.noreply.github.com>
2025-03-03 17:55:10 +0000

    loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,25 @@
+//while
+//intialization
+//while(condition){step}
+
+let index=5;
+while(index<10){
+    console.log(index);
+    index=index+2;
+}
+
+let array =["Mango","apple","banana"];
+console.log(array.length);
+index=0;
+while(index<array.length){
+    const element =array[index];
+        console.log(element);
+        index++;
+}
+
+//do whilel loop
+let score=1;
+do{
+    console.log(score)
+    score++
+}while(score<=10);
